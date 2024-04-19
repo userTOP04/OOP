7b9b2b4db3ca976b65d0df6b6ed00e9d158a38ae
userTOP04 <112961367+userTOP04@users.noreply.github.com>
2024-04-19 16:38:10 +0300

    Add files via upload

--- mytknder1.py
+++ mytknder1.py
@@ -18,7 +18,6 @@ questions = [
 class App:
     '''
     Приложение
-    TODO: Окно во весь экрон
     '''
     def __init__(self) -> None:
         self.window = tkinter.Tk()
@@ -26,20 +25,24 @@ class App:
         screen_width = self.window.winfo_screenwidth()
         screen_hight = self.window.winfo_screenheight()
         self.window.geometry(f'{screen_width}x{screen_hight}')
+        self.make_widgets()
+        self.position_widgets()
+        self.question_index = 0
+        self.window.mainloop()
         
-        
+    def make_widgets(self) -> None:
         self.question_text = tkinter.Label(self.window)
         self.question_answer_1 = tkinter.Label(self.window)
         self.question_answer_2 = tkinter.Label(self.window)
         self.question_answer_3 = tkinter.Label(self.window)
         self.question_answer_4 = tkinter.Label(self.window)
-        self.answer_button_1 = tkinter.Button(self.window, text=1, command=lambda: self.on_click(1))
-        self.answer_button_2 = tkinter.Button(self.window, text=2, command=lambda: self.on_click(2))
-        self.answer_button_3 = tkinter.Button(self.window, text=3, command=lambda: self.on_click(3))
-        self.answer_button_4 = tkinter.Button(self.window, text=4, command=lambda: self.on_click(4))
+        self.answer_button_1 = tkinter.Button(self.window, text=1, command=lambda: self.on_click(0))
+        self.answer_button_2 = tkinter.Button(self.window, text=2, command=lambda: self.on_click(1))
+        self.answer_button_3 = tkinter.Button(self.window, text=3, command=lambda: self.on_click(2))
+        self.answer_button_4 = tkinter.Button(self.window, text=4, command=lambda: self.on_click(3))
 
 
-
+    def position_widgets(self) -> None:
         self.question_text.pack()
         self.question_answer_1.pack() 
         self.question_answer_2.pack()
@@ -51,17 +54,26 @@ class App:
         self.answer_button_4.pack()
 
 
-        for question in questions:
-            self.question_text['text'] = question['вопрос']
-            self.question_answer_1['text'] = '1.' + question['ответы'][0]
-            self.question_answer_2['text'] = '2.' + question['ответы'][1]
-            self.question_answer_3['text'] = '3.' + question['ответы'][2]
-            self.question_answer_4['text'] = '4.' + question['ответы'][3]
+    def show_question(self) -> None:
+        question = questions[self.question_index]
+        self.question_text['text'] = question['вопрос']
+        self.question_answer_1['text'] = '1.' + question['ответы'][0]
+        self.question_answer_2['text'] = '2.' + question['ответы'][1]
+        self.question_answer_3['text'] = '3.' + question['ответы'][2]
+        self.question_answer_4['text'] = '4.' + question['ответы'][3]
 
-        self.window.mainloop()
+        
 
-    def on_click(self, button_number: int):
-        print('Кнопка нажата №', button_number)
+    def on_click(self, button_index: int):
+        question = questions[self.question_index]
+        if button_index == question['индекс правильного ответа']
+            print('Верно')
+        else:
+            print('НЕВЕРНО')
+        if self.question_index + 1 == len(questions) 
+            print('Вопросы викторины закончились')
+        self.question_index += 1
+        self.show_question()
 
 
         
